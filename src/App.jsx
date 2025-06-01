527314889d03dc7cadf3438310bb750c9cc5e5ef
Ilya Hvostenko <ilyajavadev@gmail.com>
2025-06-01 22:09:05 +0300

    fix: Corrected all component import file extensions to .jsx (lowercase)

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,28 @@
 // src/App.jsx
 import React, { useEffect } from "react";
-import { useTranslation } from "react-i18next"; // Импортируем useTranslation
+import { useTranslation } from "react-i18next";
 
 import './i18n'; // Импортируй свою конфигурацию i18n
 
 import "./index.css";
 import Navbar from "./components/NavBar";
-import Services from "./components/Services";
-import GlassCardShowcase from "./components/GlassCardShowcase";
-import FAQ from "./components/FAQ.JSX";
-import ContactSection from "./components/ContactSection";
-import AboutUs from "./components/AboutUs";
-import AccessibilityButton from "./components/AccessibilityButton";
-import Footer from "./components/Footer";
-import HeroSection from "./components/HeroSection";
-import CustomCursor from "./components/CustomCursor";
-import GlitchIntro from "./components/GlitchIntro";
+import Services from "./components/Services.jsx"; // ИСПРАВЛЕНО
+import GlassCardShowcase from "./components/GlassCardShowcase.jsx"; // ИСПРАВЛЕНО
+import FAQ from "./components/FAQ.jsx"; // ИСПРАВЛЕНО
+import ContactSection from "./components/ContactSection.jsx"; // ИСПРАВЛЕНО
+import AboutUs from "./components/AboutUs.jsx"; // ИСПРАВЛЕНО
+import AccessibilityButton from "./components/AccessibilityButton.jsx"; // ИСПРАВЛЕНО
+import Footer from "./components/Footer.jsx"; // Вероятно, тоже .jsx
+import HeroSection from "./components/HeroSection.jsx"; // ИСПРАВЛЕНО
+import CustomCursor from "./components/CustomCursor.jsx"; // ИСПРАВЛЕНО
+import GlitchIntro from "./components/GlitchIntro.jsx"; // ИСПРАВЛЕНО
 
 function App() {
-  const { i18n } = useTranslation(); // Инициализируем хук useTranslation
+  const { i18n } = useTranslation();
 
   useEffect(() => {
-    // Устанавливаем атрибут dir на HTML-элементе при изменении языка
-    // i18n.dir() возвращает 'rtl' для RTL языков (как 'he') и 'ltr' для LTR языков (как 'en', 'ru')
     document.documentElement.dir = i18n.dir(i18n.language);
-  }, [i18n.language]); // Эффект будет запускаться при смене языка
+  }, [i18n.language, i18n]); // Добавляем i18n в зависимости, если еще не добавлено
 
   return (
     <>
@@ -38,7 +36,7 @@ function App() {
       <FAQ />
       <ContactSection />
       <Footer />
-   
+      <AccessibilityButton /> {/* Ты импортировал его, но не использовал в JSX */}
     </>
   );
 }
